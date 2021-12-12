27e0ca0d69bf44a9d5568f2a663f4fadaef7ac86
ilhamutomo1 <ilhamtriputroutomo@gmail.com>
2021-12-12 20:25:52 +0700

    menambahkan file Contoh1.php

---
+++ app/Controllers/Contoh1.php
@@ -0,0 +1,11 @@
+<?php 
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "Perkenalkan<br>";
+        echo "Nama saya Ilham Utomo<br>";
+        echo "Hobi saya skateboard";
+    }
+}
+?>
